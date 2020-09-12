8f257c0471a4180a4a85440d8313b1fade08c5a1
Jimmy Mercado <developer@jimmymercado.com>
2020-09-11 23:21:00 -0700

    created Directory, MenuItem, and Homepage components

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import MenuItem from '../menu-item/menu-item.component';
+import './directory.styles.scss';
+
+import dir_sections from '../../data/directory.data.json';
+
+
+class Directory extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			sections:[]
+		}
+	}
+
+	componentDidMount(){
+    // fetch('http://jm.trimble.com/monsters-rolodex/directory.data.json')
+    // .then(res => res.json())
+		// .then(items => this.setState({sections:items})) //overwrite monsters
+		this.setState({sections:dir_sections.sections});
+
+  }
+
+	render() {
+		return (
+			<div className='directory-menu'>
+					{
+						this.state.sections.map(({id, title, imageUrl, size}) => (
+							<MenuItem key={id} title={title} imageUrl={imageUrl} size={size}	/>
+						))
+					}
+
+			</div>
+		)
+	}
+}
+
+export default Directory;
