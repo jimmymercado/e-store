1e56252f967206f47c2bf51038e9697092036596
Jimmy Mercado <developer@jimmymercado.com>
2020-09-23 14:10:08 -0700

    Added AddItemToCart action, refactored collection-item, collection-preview and cartReducer

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,7 +1,6 @@
-import cartReducer from "./cart.reducer"
-
 const CartActionTypes = {
-	TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
+	TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+	ADD_ITEM: 'ADD_ITEM'
 }
 
 export default CartActionTypes;
